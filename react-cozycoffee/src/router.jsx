d9253580edc072326e13dac1582575ab48dac80d
DAVID GOMEZ <DAVID_AGOMEZT@HOTMAIL.COM>
2024-01-26 18:16:54 +0100

    Add Auth Pages
    Add Home.jsx
    Add AuthLayout.jsx
    Add Login.jsx
    Add Register.jsx
    Add Images

--- react-cozycoffee/src/router.jsx
+++ react-cozycoffee/src/router.jsx
@@ -1,10 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Home from "./views/Home";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./views/Login";
+import Register from "./views/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login />,
+      },
+      {
+        path: "/auth/register",
+        element: <Register />,
+      },
+    ],
   },
 ]);
 
